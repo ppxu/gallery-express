8db942c2173b3d14a59e6ff50019bd89b27d31da
zihan <zihan.yx@taobao.com>
2013-03-15 15:47:57 +0800

    update

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path')
-  , fs = require('fs')
-  , marked = require('marked');
+var express = require('express'),
+  routes = require('./routes'),
+  http = require('http'),
+  path = require('path'),
+  fs = require('fs'),
+  marked = require('marked');
 
 var app = express();
 
-app.configure(function(){
+app.configure(function() {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -20,60 +19,52 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(require('less-middleware')({
+    src: __dirname + '/public'
+  }));
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
   app.use(express.errorHandler());
 });
 
-/*app.engine('md', function(path, options, fn){
-  fs.readFile(path, 'utf8', function(err, str){
-    if (err) return fn(err);
-    str = markdown.parse(str).toString();
-    fn(null, str);
-  });
-});
-*/
 app.get('/', routes.index);
 
 app.get('/:title/:version', function(req, res, next) {
-    var version = req.params.version;
-    var title = req.params.title;
-    var urlPath = [
-        __dirname, '/gallery/',
-        title, '/',
-        version, '/docs/',
-        title, '.md'
-    ].join('');
+  var version = req.params.version;
+  var title = req.params.title;
+  var urlPath = [
+  __dirname, '/gallery/',
+  title, '/',
+  version, '/doc/',
+  title, '.md'].join('');
 
-    var filePath = path.normalize('./' + urlPath);
+  var filePath = path.normalize('./' + urlPath);
 
-    fs.exists(filePath, function(exists){
-      if(exists){
-        var content = fs.readFileSync(filePath, 'utf-8');
-        var tokens = marked.lexer(content);
-        var html_content = marked.parser(tokens);
-        res.render('show', {
-            title: title,
-            blog_content: html_content,
-            pretty: true
-        });
-      }
-      else{
-        next();
-      }
-    });
+  fs.exists(filePath, function(exists) {
+    if (exists) {
+      var content = fs.readFileSync(filePath, 'utf-8');
+      var tokens = marked.lexer(content);
+      var html_content = marked.parser(tokens);
+      res.render('show', {
+        title: title,
+        blog_content: html_content,
+        pretty: true
+      });
+    } else {
+      next();
+    }
+  });
 });
 
 app.get('*', function(req, res) {
-    res.render('404', {
-        title: '404'
-    });
+  res.render('404', {
+    title: '404'
+  });
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
 });
