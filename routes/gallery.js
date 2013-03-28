2f7ee05d3440f97f86f152a3f76ebe623ccbb44b
zihan <zihan.yx@taobao.com>
2013-03-28 16:15:03 +0800

    update

---
+++ routes/gallery.js
@@ -0,0 +1,34 @@
+/*
+ * GET gallery docs.
+ */
+
+exports.docs = function(req, res, next) {
+	var path = require('path'),
+		fs = require('fs'),
+		marked = require('marked'),
+		version = req.params.version,
+		title = req.params.title,
+		baseUrl = process.cwd(),
+		urlPath = [
+			baseUrl, '/gallery/',
+			title, '/',
+			version, '/doc/',
+			title, '.md'].join('');
+
+	var filePath = path.normalize(urlPath);
+
+	fs.exists(filePath, function(exists) {
+		if (exists) {
+			var content = fs.readFileSync(filePath, 'utf-8');
+			var tokens = marked.lexer(content);
+			var htmlContent = marked.parser(tokens);
+			res.render('show', {
+				title: title,
+				blogContent: htmlContent,
+				pretty: true
+			});
+		} else {
+			next();
+		}
+	});
+};
